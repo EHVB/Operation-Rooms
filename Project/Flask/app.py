b115a0617a776801c6e71ff8982dfee9dffff992
EHVB <ehab.mahmoud00@eng-st.cu.edu.eg>
2022-05-28 16:24:11 +0200

    Connected HTML with eachother

--- Project/Flask/app.py
+++ Project/Flask/app.py
@@ -2,8 +2,56 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_name():
+def index():
    return render_template('index.html')
-
+########################################################
+@app.route('/home')
+def home():
+   return render_template('home.html')
+########################################################
+@app.route('/about_us')
+def about_us():
+   return render_template('about us.html')
+########################################################
+@app.route('/appointments')
+def appointments():
+   return render_template('appointments.html')
+########################################################
+@app.route('/complain')
+def complain():
+   return render_template('complain.html')
+########################################################
+@app.route('/doctors')
+def doctors():
+   return render_template('doctors.html')
+########################################################
+@app.route('/engineers')
+def engineers():
+   return render_template('engineers.html')
+########################################################
+@app.route('/login')
+def login():
+   return render_template('login.html')
+########################################################
+@app.route('/nurses')
+def nurses():
+   return render_template('nurses.html')
+########################################################
+@app.route('/patients')
+def patients():
+   return render_template('patients.html')
+########################################################
+@app.route('/rooms_info')
+def rooms_info():
+   return render_template('rooms info.html')
+########################################################
+@app.route('/sign_up')
+def sign_up():
+   return render_template('sign_up.html')
+########################################################
+@app.route('/profile')
+def profile():
+   return render_template('profile.html')
+########################################################
 if __name__ == '__main__':
    app.run()
