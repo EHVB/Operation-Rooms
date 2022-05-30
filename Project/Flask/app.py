98e1baf9ca704c9ee9b13401031c4326c4ca2fcb
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2022-05-30 22:21:30 +0200

    add login route and connect to db

--- Project/Flask/app.py
+++ Project/Flask/app.py
@@ -1,5 +1,16 @@
-from flask import Flask, render_template
+import mysql.connector
+from flask import Flask, redirect, url_for, request,render_template,session
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="admin",
+  database="datatry"
+)
+mycursor = mydb.cursor()
+
 app = Flask(__name__)
+app.secret_key = 'very secret key'
 
 @app.route('/')
 def index():
@@ -29,9 +40,32 @@ def doctors():
 def engineers():
    return render_template('engineers.html')
 ########################################################
-@app.route('/login')
+@app.route('/login',methods = ['POST', 'GET'])
 def login():
-   return render_template('login.html')
+   
+        # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' :
+       
+        # Create variables for easy access
+        user = request.form['username']
+        passw= request.form['password']
+        #print(user,passw,  file=sys.stdout, flush=True)
+        mycursor.execute('SELECT * FROM logintry WHERE username = %s AND password = %s limit 1', (user, passw))
+        # Fetch one record and return result
+        account = mycursor.fetchone()
+        if account:
+            
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['username'] = account[0]
+            #print (session['username'])
+            return redirect(url_for('home'))
+        else:
+           return redirect(url_for('login'))
+                
+    else:
+      return render_template('login.html')        
+
 ########################################################
 @app.route('/nurses')
 def nurses():
@@ -53,5 +87,14 @@ def sign_up():
 def profile():
    return render_template('profile.html')
 ########################################################
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+########################################################
+
 if __name__ == '__main__':
    app.run()
